d785b727a683c3c55987363de59f26557e22532c
ZahranDhar <zahrannazirdhar@gmail.com>
2024-07-20 21:09:26 +0530

    Add files via upload

---
+++ 26.c
@@ -0,0 +1,37 @@
+//Find sum of series: 1 + 1/2 + 1/4 + 1/6 + ….+ 1/n
+
+#include<stdio.h>
+
+int main(){
+
+    int i,n;
+    float x=1;
+
+    printf("Enter n.\n");
+    scanf("%d",&n);
+
+    if(n==1)
+    {
+        printf("The sum is 1.");
+    }
+    else
+    {
+        while(n%2!=0)
+    {
+        printf("The number is odd. Please enter even number.");
+      
+        scanf("%d",&n);
+    }
+
+    for(i=2; i<=n; i=i+2)
+    {
+        x=x+(1.0/i);
+    }
+
+    printf("The sum is %f",x);
+    }
+
+
+    return 0;
+
+}
