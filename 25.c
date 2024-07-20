3512a7f538aa619023bb8f1f7ae67843a5656025
ZahranDhar <zahrannazirdhar@gmail.com>
2024-07-20 19:34:06 +0530

    Add files via upload

---
+++ 25.c
@@ -0,0 +1,21 @@
+//Display the series: 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 ....... (n times) [using nesting of loops]
+
+#include<stdio.h>
+
+int main(){
+
+    int i,j,n;
+
+    printf("Enter n\n");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        printf("%d ",j);
+    }
+
+
+    return 0;
+
+}
